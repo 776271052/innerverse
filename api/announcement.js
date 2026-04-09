53a1afb207569cb4a6458612cccaa7074178d01b
wuliwuling <42362419+776271052@users.noreply.github.com>
2026-04-09 22:25:10 +0800

    Update announcement.js

--- api/announcement.js
+++ api/announcement.js
@@ -1,6 +1,5 @@
 import { getConfig } from './lib/db.js';
-
-export default async function handler(req, res) {
+export default async (req, res) => {
     const content = await getConfig('announcement', '');
     res.json({ success: true, data: { content } });
-}
+};
