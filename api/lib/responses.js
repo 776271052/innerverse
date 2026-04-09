421ba06bbb097dadc886d2150067cb196ee4038a
wuliwuling <42362419+776271052@users.noreply.github.com>
2026-04-10 00:26:56 +0800

    Update responses.js

--- api/lib/responses.js
+++ api/lib/responses.js
@@ -1,7 +1,1 @@
-export function success(data = null, message = 'success') {
-    return { success: true, data, message };
-}
-
-export function error(message, status = 400) {
-    return { success: false, error: message, status };
-}
+export function success(data=null,message='success'){return{success:!0,data,message}};export function error(message,status=400){return{success:!1,error:message,status}}
