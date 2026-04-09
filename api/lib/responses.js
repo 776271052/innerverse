ae4e573589e9fef1346243539bddcd6caff748ed
wuliwuling <42362419+776271052@users.noreply.github.com>
2026-04-09 23:09:11 +0800

    Delete api directory

--- api/lib/responses.js
+++
@@ -1,7 +0,0 @@
-export function success(data = null, message = 'success') {
-    return { success: true, data, message };
-}
-
-export function error(message, status = 400) {
-    return { success: false, error: message, status };
-}
