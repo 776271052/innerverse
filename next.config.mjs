4d7983934f93289cf0785bc9c85e1d9f316a0419
wuliwuling <42362419+776271052@users.noreply.github.com>
2026-04-13 04:42:49 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '**' }]
-  },
-  experimental: {
-    optimizePackageImports: ['framer-motion', 'lucide-react', 'sonner']
   }
 }
 
